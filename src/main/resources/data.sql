dcbd41a45e9e4c55214558e3bd6a63721a88b3f2
nilam-code <60399039+nilam-code@users.noreply.github.com>
2020-01-29 14:08:13 +0100

    Add files via upload

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
 CREATE SCHEMA IF NOT EXISTS SalesData;
 
 
+insert into salesdata.company(company_id,company_name,currency,description,price,prod_id,product_name,sale) values (1,'Acme corporation','GBP','Acme corporation',19.99,1,'Rocket-Powered Roller Skates',1);
